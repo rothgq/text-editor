0efc9180c274232f95fcfcd03c372a0c1e9a7df9
rothgq <rothgq@gmail.com>
2024-02-12 15:35:27 -0500

    Updates to commands in package.json and completed files in client folder.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,26 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (id, value) => {
+  console.log('Update to the jateDB');
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id: id, value: value});
+  const result = await request;
+  console.log('Data updated in jateDB', result);
+};
+
+export const getDb = async () => {
+  console.log('Fetching from jateDB');
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('Fetch all data from jateDB complete', result);
+  return result;
+};
 
 initdb();
